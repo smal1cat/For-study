0469e55002305dcc2a4049179b3c825ca06fba16
TaeHyung Kim <gud3793@naver.com>
2024-05-22 22:14:10 +0900

    11 commit

---
+++ 5월22일.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if (i%m==c-1){
+                answer += my_string.charAt(i);
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
